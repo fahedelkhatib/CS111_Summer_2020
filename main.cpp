9bc2a42b754e99641cae8e39d308a95c51ba9c7d
fahedelkhatib <felkh002@ucr.edu>
2020-07-17 22:14:50 +0000

    imperfect encryption algorithm

--- main.cpp
+++ main.cpp
@@ -5,41 +5,185 @@
 #include <iomanip>
 using namespace std;
 
-long long int p, q, e, d, n, m, encmess, totient, result;
-int decryptedmessage[1000];
+long long int p, q, e, d, m, encmess, totient, result, encryptionNum, publickey;
+unsigned long long int decryptedmessage[5000];
 bool flag = 0;
-long int factorthatnigga(long int n);
-int breakandDecrypt(int e, int n, int m);
+int breakandDecrypt(int e, int n, int m, int p, int q);
 void printMessage(int number);
-
-int main(){
-	
-	//long int p, q, e, d, n, m, encmess;
-	//char message[1000], encmessage[1000], decmessage[1000];
-	
-	cout << "I work, for the most part!\n";
-	cout << "now then... what's all this?\n";
-	
-	int encryptedMessage[] = {10, 7, 58, 30, 23, 62, 7, 64, 62, 23, 62, 61, 7, 41, 62, 21, 7, 49, 
+void encodeMessage(string message);
+void encryptMessage(int n, int e);
+unsigned long long int unencmess[5000];
+unsigned long long int encmessage[5000];
+int encryptedMessage[] = {10, 7, 58, 30, 23, 62, 7, 64, 62, 23, 62, 61, 7, 41, 62, 21, 7, 49, 
 					75, 7, 69, 53, 58, 37, 37, 41, 10, 64, 50, 7, 10, 64, 21, 62,
 					61, 35, 62, 61, 62, 7, 52, 10, 21, 58, 7, 49, 75, 7, 62, 26, 
 					22, 53, 30, 21, 10, 37, 64};
+	
+int main(){
+	
+	encodeMessage("abcdefghij klmnopqrstuvwxyz");
+
+	cout << "encoded message: ";
+	int j = 0;
+	while(unencmess[j] > 0){
+		cout << unencmess[j] << " ";
+		++j;
+	}
+	cout << endl;
+	
+	p = 7;
+	q = 11;
+	int n = p * q;
+	e = 13;
+
+	encryptMessage(n, e);
+
 	int i = 0;
 	while(encryptedMessage[i] > 0){
 		
-		decryptedmessage[i] = breakandDecrypt(13, 77, encryptedMessage[i]);
+		decryptedmessage[i] = breakandDecrypt(e, n, encmessage[i], p, q);
 		++i;
 	}
 
-	//for(int i = 0; i < 57; ++i){
-	//	cout << decryptedmessage[i] << " ";
-	//}
-	for(int i = 0; i < 57; ++i){
+	for(int i = 0; i < sizeof(encryptedMessage); ++i){
 		int var = decryptedmessage[i];
 		printMessage(var);
 	}
+	cout << endl;
 
 }
+
+void encryptMessage(int n, int e){
+	
+	cout << "uh" << endl;
+	cout << "size of encryptedMessage[]: " << sizeof(encryptedMessage) << endl;
+	
+	int i = 0;
+	cout << "e = " << e << endl;
+	cout << "n = " << n << endl;
+	while(unencmess[i] > 0){
+		
+		//cout << "encrypting character: " << i;
+		//cout << "\t" << unencmess[i] << "^" << e << "mod " << n << endl;
+		long double powresult = pow(unencmess[i], e);
+		unsigned long long int powresultint = powresult;
+
+		encmessage[i] = powresultint % n;
+		//cout << "\tresult: " << powresultint % n << endl;
+		//cout << " DONE" << endl;
+		++i;
+	}
+	cout << "finished encryption" << endl;
+
+	cout << "new message: ";
+
+	i = 0;
+	while (encmessage[i] > 0){
+		
+		cout << encmessage[i] << " ";
+		++i;
+	}
+	cout << endl;
+}
+
+
+void encodeMessage(string message){
+
+	for(int i = 0; i < message.size(); ++i){
+		
+		switch(message[i]){
+
+			case 'a':
+				unencmess[i] = 2;
+				break;
+			case 'b':
+				unencmess[i] = 3;
+				break;
+			case 'c':
+				unencmess[i] = 4;
+				break;
+			case 'd':
+				unencmess[i] = 5;
+				break;
+			case 'e':
+				unencmess[i] = 6;
+				break;
+			case 'f':
+				unencmess[i] = 7;
+				break;
+			case 'g':
+				unencmess[i] = 8;
+				break;
+			case 'h':
+				unencmess[i] = 9;
+				break;
+			case 'i':
+				unencmess[i] = 10;
+				break;
+			case 'j':
+				unencmess[i] = 11;
+				break;
+			case 'k':
+				unencmess[i] = 12;
+				break;
+			case 'l':
+				unencmess[i] = 13;
+				break;
+			case 'm':
+				unencmess[i] = 14;
+				break;
+			case 'n':
+				unencmess[i] = 15;
+				break;
+			case 'o':
+				unencmess[i] = 16;
+				break;
+			case 'p':
+				unencmess[i] = 17;
+				break;
+			case 'q':
+				unencmess[i] = 18;
+				break;
+			case 'r':
+				unencmess[i] = 19;
+				break;
+			case 's':
+				unencmess[i] = 20;
+				break;
+			case 't':
+				unencmess[i] = 21;
+				break;
+			case 'u':
+				unencmess[i] = 22;
+				break;
+			case 'v':
+				unencmess[i] = 23;
+				break;
+			case 'w':
+				unencmess[i] = 24;
+				break;
+			case 'x':
+				unencmess[i] = 25;
+				break;
+			case 'y':
+				unencmess[i] = 26;
+				break;
+			case 'z':
+				unencmess[i] = 27;
+				break;
+			default:
+				unencmess[i] = 28;
+				break;
+
+		}
+	}
+}
+
+
+
+
+
+
 
 void printMessage(int number){
 
@@ -127,23 +271,22 @@ void printMessage(int number){
 				 cout << " ";
 				 break;
 			default: 
-				 cout << "----FFFFUUUUUCCCCKKKKKK----" << endl;
 				 break;
 		
 		}
 }
 
 
-int breakandDecrypt(int e, int n, int encmess){	
+int breakandDecrypt(int e, int n, int encmess, int p, int q){	
 	e = e;
 	n = n;
 	m = encmess;
-	p = 7;
-	q = 11;
+	p = p;
+	q = q;
 	
 	totient = (p - 1) * (q - 1);
 	
-	for (int i = 2; i < 10000; ++i){
+	for (int i = 2; i < 1000000; ++i){
 		
 		if((e * i) % totient == 1){
 			d = i;
@@ -157,21 +300,14 @@ int breakandDecrypt(int e, int n, int encmess){
 		cout << "decryption failed" << endl;
 	}
 	
-	//cout << "\tdecrypting message proper" << endl;
-	//cout << "encmess = " << m << endl;
-	//cout << "d = " << d << endl;
-	//cout << "n = " << n << endl;
-	
 	result = 1;
+	
 	for(int i = 1; i <= d; ++i){
 
 		result = (result * m) % n;
 	       	//cout << result << endl;	
 		
 	}
-	
-	cout << "\tdecrypted message: " << result << endl;
-	cout << "\tencrypted message: " << m << endl << endl;	
 	
 	return result;
 }	
--- sample.cpp
+++ sample.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-
+char* encryptROT13(char word[], int encryptionNum);
 
 int main(){
+	
+	int arr1[] = {3, 4, 4, 3, 4, 2, 3, 4, 3, 2};
+	int arr2[] = {4, 2, 3, 4, 2, 3, 4, 5, 3, 4, 5, 3, 4, 6, 4, 5};
+	int ans[25];//sizeof(arr1) + sizeof(arr2)];
+	
+	for(int i=0;i<10;i++)
+	{
+		for(int j=0; j<15;j++) {
+			ans[i+j]+=arr1[i]*arr2[j];
+			ans[i+j+1]=ans[i+j+1]+ans[i+j]/10;
+			ans[i+j]%=10;
+		}
+	}
+	cout << endl;
 
-	cout << (26*26) % 77 << endl;
+	double i = 13;
+	double j = 21;
+	double res = pow(j, i);
 
+
+	cout << pow(j, i) << endl;
+	
+	unsigned long long int resdub = res;
+	
+	cout << j << "^" << i << " mod 77" << endl;
+
+	unsigned long long int result = resdub % 77;
+	cout << "result: " << result << endl;
+	
 	return 0;
 }
